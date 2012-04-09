6edcfe437a8df607d29f55fd83356898c1a1a6ba
bobchin <bobchin.ryu@gmail.com>
2012-04-09 18:51:35 +0900

    1-2の追加とディレクトリの変更

---
+++ chapter1/list0108.c
@@ -0,0 +1,17 @@
+/*
+ * 2 つの変数に整数値を格納して表示
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int vx, vy;
+
+    vx = 57;
+    vy = vx + 10;
+
+    printf("vx の値は %d です。\n", vx);
+    printf("vy の値は %d です。\n", vy);
+
+    return (0);
+}
