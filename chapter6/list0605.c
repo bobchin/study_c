a056f33602557bdb409d871ca64d150c32a6ab86
bobchin <bobchin.ryu@gmail.com>
2015-09-18 13:53:48 +0900

    List 6-5

---
+++ chapter6/list0605.c
@@ -0,0 +1,26 @@
+/*
+ * べき乗を求める
+ */
+#include <stdio.h>
+
+double power(double dx, int no)
+{
+  double tmp = 1.0;
+
+  while (no-- > 0)
+    tmp *= dx;
+  return tmp;
+}
+
+int main(void)
+{
+  int n;
+  double x;
+
+  printf("実数を入力してください："); scanf("%lf", &x);
+  printf("整数を入力してください："); scanf("%d", &n);
+
+  printf("%.2fの%d乗は%.2fです。\n", x, n ,power(x, n));
+
+  return 0;
+}
