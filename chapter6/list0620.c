c5e8745f886bf64b36111c39185de6b38d93a46d
bobchin <bobchin.ryu@gmail.com>
2015-09-20 13:39:35 +0900

    List 6-20

---
+++ chapter6/list0620.c
@@ -0,0 +1,18 @@
+/*
+ * 暗黙の初期化を確認
+ */
+#include <stdio.h>
+
+int fx;
+
+int main(void)
+{
+  static int sx;
+  int        ax;
+
+  printf("ax = %d\n", ax);
+  printf("sx = %d\n", sx);
+  printf("fx = %d\n", fx);
+
+  return 0;
+}
