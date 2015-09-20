67c5ecb471d4e3cd44080029d59964597e59d693
bobchin <bobchin.ryu@gmail.com>
2015-09-20 13:46:50 +0900

    List 7-1

---
+++ chapter7/list0701.c
@@ -0,0 +1,18 @@
+/*
+ * 本書で想定する<limit.h>の一部
+ */
+
+#define UCHAR_MAX 255U
+#define SCHAR_MIN -128
+#define SCHAR_MAX +127
+#define CHAR_MIN  0
+#define CHAR_MAX  UCHAR_MAX
+#define SHRT_MIN  -32768
+#define SHRT_MAX  +32767
+#define USHRT_MAX 65535U
+#define INT_MIN   -32768
+#define INT_MAX   +32767
+#define UINT_MAX  65535U
+#define LONG_MIN  -2147483648L
+#define LONG_MAX  +2147483647L
+#define ULONG_MAX 4294967295UL
