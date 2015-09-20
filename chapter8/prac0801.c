46f5eefc6261b6257ebd173fb0cd6246fb3a5f61
bobchin <bobchin.ryu@gmail.com>
2015-09-20 22:55:14 +0900

    演習 8-1

---
+++ chapter8/prac0801.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define diff(x, y)  ((x > y)? x - y: y - x)
+
+int main(void)
+{
+  int nx, ny;
+
+  printf("整数を入力してください：");
+  scanf("%d", &nx);
+  printf("整数を入力してください：");
+  scanf("%d", &ny);
+
+  printf("その数の差は%dです。\n", diff(nx, ny));
+
+  return 0;
+}
